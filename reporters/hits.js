6ea652e4b4913a066897b99fd0b0147879462143
John Jones <JJones@adfero.com>
2015-09-02 18:06:42 -0400

    totally committing broken code here

---
+++ reporters/hits.js
@@ -0,0 +1,46 @@
+var googleanalytics = require('../lib/googleanalytics');
+
+exports = {
+  'name': 'hits',
+  'average': function(data,done) {
+    googleanalytics.getHitsPerPath(
+      data.google.token,
+      data.google.profile,
+      data.sampleStart,
+      data.sampleEnd,
+      null,
+      function(err,data) {
+      if (err) {
+        return done(err);
+      } else {
+        var validUrls = data.rows.reduce(function(previous,current) {
+          return previous + ((!data.pattern || data.pattern.match(current[0])) ? 1 : 0);
+        },0)
+        var average = data.rows.reduce(function(previous,current) {
+          return previous + ((!data.pattern || data.pattern.match(current[0])) ? parseFloat(current[1]) : 0);
+        },0.0) / parseFloat(validUrls);
+        return callback(null,average);
+      }
+    })
+  },
+  'page': function(data,done) {
+    googleanalytics.getHitsPerPath(
+      data.google.token,
+      data.google.profile,
+      data.reportStart,
+      data.reportEnd,
+      data.urls,
+      function(err,data) {
+      if (err) {
+        callback(err);
+      } else {
+        callback(null,data.rows.map(function(row) {
+          return {
+            'path': row[0],
+            'hits': parseFloat(row[1])
+          }
+        }));
+      }
+    })
+  }
+}
--- routes/report.js
+++ routes/report.js
@@ -1,6 +1,7 @@
-var googleanalytics = require('../lib/googleanalytics');
 var url = require('url');
 var UrlPattern = require('url-pattern');
+var reporters = require('../reporters').reporters;
+var async = require('async');
 
 exports.form = function(req,res) {
   res.render('report',{
@@ -9,61 +10,55 @@ exports.form = function(req,res) {
 }
 
 exports.build = function(req,res,next) {
-  var sampleStart = new Date(req.body['sample-start']);
-  var sampleEnd = new Date(req.body['sample-end']);
-  var reportStart = new Date(req.body['report-start']);
-  var reportEnd = new Date(req.body['report-end']);
-  var urls = req.body['report-urls'].split('\n').map(function(urlStr) {
-    return url.parse(urlStr).path;
-  });
   // googleanalytics.getSocialPostsPerPath(req.session.auth.google,req.body['google-profile'],sampleStart,sampleEnd,['/insights/2015/08/an-event-apart-2015'],function(err,data) {
   //   console.log(data);
-  // })
-  buildAverages(req.session.auth.google,req.body['google-profile'],sampleStart,sampleEnd,req.body['path-pattern'],function(err,averageHits) {
-    buildReportActuals(req.session.auth.google,req.body['google-profile'],reportStart,reportEnd,urls,function(err,actuals) {
-      actuals.forEach(function(actual) {
-        actual.hitsScore = actual.hits / averageHits;
-        return actual;
-      });
-      res.send(actuals);
-    });
-  })
-}
-
-function buildAverages(token,profile,sampleStart,sampleEnd,pattern,callback) {
-  if (!pattern || (pattern && pattern.trim().length == 0)) {
-    pattern = false;
-  } else {
-    pattern = new UrlPattern(pattern);
+  // });
+  var data = {
+    'google': {
+      'token': req.session.auth.google,
+      'profile': req.body['google-profile']
+    },
+    'sampleStart': new Date(req.body['sample-start']),
+    'sampleEnd': new Date(req.body['sample-end']),
+    'reportStart': new Date(req.body['report-start']),
+    'reportEnd': new Date(req.body['report-end']),
+    'pattern': (!req.body['path-pattern'] || (req.body['path-pattern'] && req.body['path-pattern'].trim().length == 0)) ? false : new UrlPattern(req.body['path-pattern']),
+    'urls': req.body['report-urls'].split('\n').map(function(urlStr) {
+      return url.parse(urlStr).path;
+    })
   }
-  googleanalytics.getHitsPerPath(token,profile,sampleStart,sampleEnd,null,function(err,data) {
-    if (err) {
-      console.error(err);
-      next(err);
-    } else {
-      var validUrls = data.rows.reduce(function(previous,current) {
-        return previous + ((!pattern || pattern.match(current[0])) ? 1 : 0);
-      },0)
-      var average = data.rows.reduce(function(previous,current) {
-        return previous + ((!pattern || pattern.match(current[0])) ? parseFloat(current[1]) : 0);
-      },0.0) / parseFloat(validUrls);
-      callback(null,average);
-    }
-  })
-}
 
-function buildReportActuals(token,profile,reportStart,reportEnd,paths,callback) {
-  googleanalytics.getHitsPerPath(token,profile,reportStart,reportEnd,paths,function(err,data) {
-    if (err) {
-      console.error(err);
-      next(err);
-    } else {
-      callback(null,data.rows.map(function(row) {
-        return {
-          'path': row[0],
-          'hits': parseFloat(row[1])
-        }
-      }));
+  async.waterfall(
+    [
+      function(callback) {
+        callback(null, data, {
+          'averages': {},
+          'page': {}
+        });
+      }
+    ].concat(
+      reporters.map(function(reporter) {
+        return function(inData, outData, callback) {
+          async.parallel({
+            'average': function(callback) {
+              reporter.average(inData,callback);
+            },
+            'page': function(callback) {
+              reporter.page(inData,callback);
+            }
+          },function(err,results) {
+            if (err) {
+              return callback(err);
+            } else {
+              outData.averages[reporter.name] = results.average;
+              outData.page[reporter.name] = results.page;
+            }
+          });
+        };
+      })
+    ),
+    function(err,result) {
+      res.send(result);
     }
-  })
+  );
 }
